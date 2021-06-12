ec0f3d78c0605798e423e9ded0f5631d167b0b5c
nucklehead <pierevans@gmail.com>
2021-06-12 17:05:32 -0400

    Ajoute workflow

--- tests/test_tichaiti_live_github_actions.py
+++ tests/test_tichaiti_live_github_actions.py
@@ -1,5 +1,12 @@
+import unittest
 from tichaiti_live_github_actions import __version__
+from tichaiti_live_github_actions.main import koman_ou_rele
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestMwenAn(unittest.TestCase):
+    def test_vesyon(self):
+        self.assertEqual(__version__, '0.1.0')
+
+    def test_non(self):
+        non = koman_ou_rele()
+        self.assertEqual(non, 'tichaiti')
---
+++ tichaiti_live_github_actions/main.py
@@ -0,0 +1,7 @@
+def koman_ou_rele():
+    return 'tichaiti'
+
+
+if __name__ == '__main__':
+    non = koman_ou_rele()
+    print(non)
